bba1c3ef44661828976ace4164e53f22a0b1237a
ilyagolubev <ilijagolubev.com>
2022-02-20 02:36:07 +0300

    added gallery

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,19 @@ $(function () {
     dots: true,
     autoplay: true,
     autoplaySpeed: 7000,
-    speed: 800,
+    speed: 500,
+  });
+  $('.gallery__content-items').slick({
+    arrows: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    speed: 500,
+    centerMode: true,
+    variableWidth: true,
+    appendArrows: '.gallery__arrows',
+    prevArrow: '<button type="button" class="gallery__slick-btn gallery__slick-prev"><img src="images/gallery/prev.svg" alt="Предыдущий слайд"></button>',
+    nextArrow: '<button type="button" class="gallery__slick-btn gallery__slick-next"><img src="images/gallery/next.svg" alt="Следующий слайд"></button>',
   });
   function getTimeRemaining(endtime) {
     const total = Date.parse(endtime) - Date.parse(new Date());
