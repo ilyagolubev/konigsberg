74cbb9107eddda35029e3c3d92c2789661208ad1
ilyagolubev <ilijagolubev.com>
2022-02-23 20:40:07 +0300

    trying to fix github

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,7 @@ $(function () {
     autoplaySpeed: 7000,
     speed: 500,
   });
+<<<<<<< HEAD
   $('.gallery__content-items').slick({
     arrows: true,
     dots: false,
@@ -17,5 +18,58 @@ $(function () {
     appendArrows: '.gallery__arrows',
     prevArrow: '<button type="button" class="gallery__slick-btn gallery__slick-prev"><img src="images/gallery/prev.svg" alt="Предыдущий слайд"></button>',
     nextArrow: '<button type="button" class="gallery__slick-btn gallery__slick-next"><img src="images/gallery/next.svg" alt="Следующий слайд"></button>',
+=======
+  function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+  function initializeClock(id, endtime) {
+    const clock = document.getElementById(id);
+    const daysSpan = clock.querySelector('.days');
+    const hoursSpan = clock.querySelector('.hours');
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+
+  const deadline = new Date('08.01.2022');
+  initializeClock('timer', deadline);
+  
+  $("[data-fancybox]").fancybox({
+    thumbs: false,
+    hash: false,
+    loop: true,
+    keyboard: true,
+    toolbar: false,
+    animationEffect: false,
+    arrows: true,
+    clickContent: false
+>>>>>>> 1cc545e655bafdfbe0d8aa1bfc88b19ddb07e4d1
   });
 })
