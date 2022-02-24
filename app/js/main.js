944ec56262863c0b91b52ea504be3111bd624d3c
ilyagolubev <ilijagolubev.com>
2022-02-25 00:05:07 +0300

    added reviews

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,18 @@ $(function () {
     autoplaySpeed: 7000,
     speed: 500,
   });
+  $('.reviews__items').slick({
+    arrows: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    speed: 500,
+    variableWidth: true,
+    centerMode: true,
+    appendArrows: '.reviews__arrows',
+    prevArrow: '<button type="button" class="reviews__slick-btn reviews__slick-prev"><img src="images/reviews/prev.svg" alt="Предыдущий слайд"></button>',
+    nextArrow: '<button type="button" class="reviews__slick-btn reviews__slick-next"><img src="images/reviews/next.svg" alt="Следующий слайд"></button>',
+  });
 
   $("[data-fancybox]").fancybox({
     thumbs: false,
