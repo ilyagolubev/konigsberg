1317150f9130ac80f0b06472891d2d0ac79af3f5
ilyagolubev <ilijagolubev.com>
2022-04-04 16:55:14 +0300

    remade reviews

--- app/js/main.js
+++ app/js/main.js
@@ -110,4 +110,20 @@ $(function () {
       respectDisabledReadOnly: false
       }); */
   $('.buy__form-ticket-amount-input').styler();
+  
+  $("form").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function () {
+      alert("Thank you!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
 })  
